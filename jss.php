e6699c4227f02735c8823c36c035d59a44e75d63
ibcker <happymiyu@gmail.com>
2013-08-29 14:09:29 +0800

    增加SAE支持

--- jss.php
+++ jss.php
@@ -1,56 +1,562 @@
-<?
-require 'jsmin.php';
+<?php
 
-define(DIR_CACHE, './cache/'); //cache files dir
-define(DIR_JS, './jss/');//jss files dir
+  /**
+    by iBcker 2013 8 29 
+  **/
 
-header("Content-Type: text/javascript; charset=UTF-8");
+	define(FILE_DIR_ARR, 'jss/,Public/js/');
+	define(DEF_DIR_ARR, 'jss/');
 
-$params=trim($_GET['jss']);//get js file list
-$list=explode(',',$params);
-foreach ($list as $key => &$value) {
-	if (!trim($value)) {
-		unset($list[$key]);
-	}else{
-		$value=eregi_replace('\.+/', '', $value);
-	}
-}
+	define(CACHE_DIR,dirname(__FILE__).'/cache/');
 
-$product=md5(json_encode($list)).'.js';//create product file name for cache
+  define(IS_SAE,isset($_SERVER['HTTP_APPNAME']));
+	
+	header("Content-Type: text/javascript; charset=UTF-8");
 
-$path=DIR_CACHE.$product;
-if (file_exists($path)) {//if exist product cache
-	$contents=file_get_contents($path);
-	echo $contents;
-}else{
-	$contents='';
-	foreach ($list as $file) {
-		$path=DIR_CACHE.md5($file).'.js';
-		if (file_exists($path)) {//if exist file cache
-			$contents.=file_get_contents($path);
+	$params=trim($_GET['files']);//get js file list
+	$list=explode(',',$params);
+	foreach ($list as $key => &$value) {
+		$value=trim($value,'./ 	
+			');
+		if (!$value||!preg_match('/\.js$/', $value)) {
+			unset($list[$key]);
 		}else{
-			$path=DIR_JS.$file;
-			if(file_exists($path)){
-				$temp=JSMin::minify(file_get_contents($path));
-				write2cache($temp,$file);
-				$contents.=$temp;
-			}
+			if (strpos($value,'/')===false) {
+				$value=DEF_DIR_ARR.$value;
+	    	}
+			ddotfilter($value);
 		}
 	}
-	if ($contents) {
-		write2cache($contents,$product);
+	
+	// var_dump($list);
+	// echo '<hr/>';
+    $product;
+    if (count($list)==1) {
+        $product=$list[0];
+    }else if(count($list)>1){
+        $product=json_encode($list);//create product file name for cache
+    }else {
+        return;
+    }
+
+	if ($contents=readCache($product)) {//if exist product cache
 		echo $contents;
+	}else{
+		$contents='';
+		foreach ($list as $file) {
+			// echo '------>'.$file.'<hr/>';
+			if ($con=readCache($file)) {//if exist file cache
+				$contents.=$con.';';
+			}else{
+				if($con=readJsFile($file)){
+					$temp=JSMin::minify($con);
+					write2cache($file,$temp);
+					$contents.=$temp.';';
+				}
+			}
+		}
+		echo $contents;
+		if ($contents)write2cache($product,$contents);
 	}
-}
 
 
-function write2cache($contents,$name){
-	$name=md5($name).'.js';
-	$fh=fopen(DIR_CACHE.$name,"w");
-	if ($fh) {
-		fwrite($fh, trim($contents));
-		fclose($fh);
-		return true;
+
+
+	function getDirList($str){
+		$list=explode(',', $str);
+		return array_values(array_diff($list, array('',NULL)));
 	}
-	return false;
+
+	//过滤路径中的..，防止非法访问上层目录
+	function ddotfilter(&$value){
+		$value=preg_replace('/\.+\//', '', $value);
+		$value=preg_replace('/\/+\//', '/', $value);
+		return $path;
+	}
+
+	function hashJsName($str){
+		return md5($str).'.js';
+	}
+
+	function readJsFile($path){
+		ddotfilter($path);
+		if (checkSafeDir($path)) {
+			$localPath=dirname(__FILE__).'/'.$path;
+			// echo 'read file:'.$localPath.'<br/>';
+			$c=file_get_contents($localPath);
+			if ($c) {
+				return trim($c);
+			}else{
+				return false;
+			}
+		}else{
+			// echo 'not safe file:'.$path.'<br/>';
+			return false;
+		}
+	}
+
+	function checkSafeDir($path){
+		$path=trim($path);
+		$dirs=getDirList(FILE_DIR_ARR);
+		if (!is_array($dirs)||empty($dirs)) {
+			return false;
+		}else{
+			foreach ($dirs as $item) {
+				if (strpos($path,$item)===0) {
+					return true;
+		    	}
+			}
+			return false;
+		}
+	}
+
+	function readCache($path){
+		$name=hashJsName($path);
+		// echo 'read cache :'.$name.'<br/>';
+		if (IS_SAE) {
+			return memcache($name);
+		}else{
+			return filecache($name);
+		}
+	}
+
+	function write2cache($path,$contents){
+		$name=hashJsName($path);
+		// echo 'write cache :'.$name.'<br/>';
+		if (IS_SAE) {
+			return memcache($name,$contents);
+		}else{
+			return filecache($name,$contents);
+		}
+	}
+
+	function memcache($path,$contents=NULL){
+		$mmc=memcache_init();
+		if($mmc==false)die("mc init failed");
+		if ($contents) {
+      // echo '写入缓存'.$path.'<br>';
+			return memcache_set($mmc,$path,$contents);
+		}else{
+      // if (memcache_get($mmc,$path)) {
+      //   echo '读取到缓存'.$path.'<br>';
+      // }else{
+      //   echo '读取不到缓存'.$path.'<br>';
+      // }
+			return memcache_get($mmc,$path);
+		}
+	}
+
+
+	function filecache($path,$contents=NULL){
+		if ($contents) {//write
+			$fh=fopen(CACHE_DIR.$path,'w');
+			// echo CACHE_DIR.$path;
+			if ($fh) {
+				$res=fwrite($fh,$contents);
+				fclose($fh);
+				return $res;
+			}
+			return false;
+		}else{//read
+			$dataPath=CACHE_DIR.$path;
+			// echo 'read file:'.$dataPath.'<br/>';
+			$c=@file_get_contents($dataPath);
+			if ($c) {
+				return trim($c);
+			}else{
+				return false;
+			}
+		}
+
+	}
+
+
+
+
+/**
+ * jsmin.php - PHP implementation of Douglas Crockford's JSMin.
+ *
+ * This is pretty much a direct port of jsmin.c to PHP with just a few
+ * PHP-specific performance tweaks. Also, whereas jsmin.c reads from stdin and
+ * outputs to stdout, this library accepts a string as input and returns another
+ * string as output.
+ *
+ * PHP 5 or higher is required.
+ *
+ * Permission is hereby granted to use this version of the library under the
+ * same terms as jsmin.c, which has the following license:
+ *
+ * --
+ * Copyright (c) 2002 Douglas Crockford  (www.crockford.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * The Software shall be used for Good, not Evil.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * --
+ *
+ * @package JSMin
+ * @author Ryan Grove <ryan@wonko.com>
+ * @copyright 2002 Douglas Crockford <douglas@crockford.com> (jsmin.c)
+ * @copyright 2008 Ryan Grove <ryan@wonko.com> (PHP port)
+ * @copyright 2012 Adam Goforth <aag@adamgoforth.com> (Updates)
+ * @license http://opensource.org/licenses/mit-license.php MIT License
+ * @version 1.1.2 (2012-05-01)
+ * @link https://github.com/rgrove/jsmin-php
+ */
+
+class JSMin {
+  const ORD_LF            = 10;
+  const ORD_SPACE         = 32;
+  const ACTION_KEEP_A     = 1;
+  const ACTION_DELETE_A   = 2;
+  const ACTION_DELETE_A_B = 3;
+
+  protected $a           = '';
+  protected $b           = '';
+  protected $input       = '';
+  protected $inputIndex  = 0;
+  protected $inputLength = 0;
+  protected $lookAhead   = null;
+  protected $output      = '';
+
+  // -- Public Static Methods --------------------------------------------------
+
+  /**
+   * Minify Javascript
+   *
+   * @uses __construct()
+   * @uses min()
+   * @param string $js Javascript to be minified
+   * @return string
+   */
+  public static function minify($js) {
+    $jsmin = new JSMin($js);
+    return $jsmin->min();
+  }
+
+  // -- Public Instance Methods ------------------------------------------------
+
+  /**
+   * Constructor
+   *
+   * @param string $input Javascript to be minified
+   */
+  public function __construct($input) {
+    $this->input       = str_replace("\r\n", "\n", $input);
+    $this->inputLength = strlen($this->input);
+  }
+
+  // -- Protected Instance Methods ---------------------------------------------
+
+  /**
+   * Action -- do something! What to do is determined by the $command argument.
+   *
+   * action treats a string as a single character. Wow!
+   * action recognizes a regular expression if it is preceded by ( or , or =.
+   *
+   * @uses next()
+   * @uses get()
+   * @throws JSMinException If parser errors are found:
+   *         - Unterminated string literal
+   *         - Unterminated regular expression set in regex literal
+   *         - Unterminated regular expression literal
+   * @param int $command One of class constants:
+   *      ACTION_KEEP_A      Output A. Copy B to A. Get the next B.
+   *      ACTION_DELETE_A    Copy B to A. Get the next B. (Delete A).
+   *      ACTION_DELETE_A_B  Get the next B. (Delete B).
+  */
+  protected function action($command) {
+    switch($command) {
+      case self::ACTION_KEEP_A:
+        $this->output .= $this->a;
+
+      case self::ACTION_DELETE_A:
+        $this->a = $this->b;
+
+        if ($this->a === "'" || $this->a === '"') {
+          for (;;) {
+            $this->output .= $this->a;
+            $this->a       = $this->get();
+
+            if ($this->a === $this->b) {
+              break;
+            }
+
+            if (ord($this->a) <= self::ORD_LF) {
+              throw new JSMinException('Unterminated string literal.');
+            }
+
+            if ($this->a === '\\') {
+              $this->output .= $this->a;
+              $this->a       = $this->get();
+            }
+          }
+        }
+
+      case self::ACTION_DELETE_A_B:
+        $this->b = $this->next();
+
+        if ($this->b === '/' && (
+            $this->a === '(' || $this->a === ',' || $this->a === '=' ||
+            $this->a === ':' || $this->a === '[' || $this->a === '!' ||
+            $this->a === '&' || $this->a === '|' || $this->a === '?' ||
+            $this->a === '{' || $this->a === '}' || $this->a === ';' ||
+            $this->a === "\n" )) {
+
+          $this->output .= $this->a . $this->b;
+
+          for (;;) {
+            $this->a = $this->get();
+
+            if ($this->a === '[') {
+              /*
+                inside a regex [...] set, which MAY contain a '/' itself. Example: mootools Form.Validator near line 460:
+                  return Form.Validator.getValidator('IsEmpty').test(element) || (/^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]\.?){0,63}[a-z0-9!#$%&'*+/=?^_`{|}~-]@(?:(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)*[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\])$/i).test(element.get('value'));
+              */
+              for (;;) {
+                $this->output .= $this->a;
+                $this->a = $this->get();
+
+                if ($this->a === ']') {
+                    break;
+                } elseif ($this->a === '\\') {
+                  $this->output .= $this->a;
+                  $this->a       = $this->get();
+                } elseif (ord($this->a) <= self::ORD_LF) {
+                  throw new JSMinException('Unterminated regular expression set in regex literal.');
+                }
+              }
+            } elseif ($this->a === '/') {
+              break;
+            } elseif ($this->a === '\\') {
+              $this->output .= $this->a;
+              $this->a       = $this->get();
+            } elseif (ord($this->a) <= self::ORD_LF) {
+              throw new JSMinException('Unterminated regular expression literal.');
+            }
+
+            $this->output .= $this->a;
+          }
+
+          $this->b = $this->next();
+        }
+    }
+  }
+
+  /**
+   * Get next char. Convert ctrl char to space.
+   *
+   * @return string|null
+   */
+  protected function get() {
+    $c = $this->lookAhead;
+    $this->lookAhead = null;
+
+    if ($c === null) {
+      if ($this->inputIndex < $this->inputLength) {
+        $c = substr($this->input, $this->inputIndex, 1);
+        $this->inputIndex += 1;
+      } else {
+        $c = null;
+      }
+    }
+
+    if ($c === "\r") {
+      return "\n";
+    }
+
+    if ($c === null || $c === "\n" || ord($c) >= self::ORD_SPACE) {
+      return $c;
+    }
+
+    return ' ';
+  }
+
+  /**
+   * Is $c a letter, digit, underscore, dollar sign, or non-ASCII character.
+   *
+   * @return bool
+   */
+  protected function isAlphaNum($c) {
+    return ord($c) > 126 || $c === '\\' || preg_match('/^[\w\$]$/', $c) === 1;
+  }
+
+  /**
+   * Perform minification, return result
+   *
+   * @uses action()
+   * @uses isAlphaNum()
+   * @uses get()
+   * @uses peek()
+   * @return string
+   */
+  protected function min() {
+    if (0 == strncmp($this->peek(), "\xef", 1)) {
+        $this->get();
+        $this->get();
+        $this->get();
+    } 
+
+    $this->a = "\n";
+    $this->action(self::ACTION_DELETE_A_B);
+
+    while ($this->a !== null) {
+      switch ($this->a) {
+        case ' ':
+          if ($this->isAlphaNum($this->b)) {
+            $this->action(self::ACTION_KEEP_A);
+          } else {
+            $this->action(self::ACTION_DELETE_A);
+          }
+          break;
+
+        case "\n":
+          switch ($this->b) {
+            case '{':
+            case '[':
+            case '(':
+            case '+':
+            case '-':
+            case '!':
+            case '~':
+              $this->action(self::ACTION_KEEP_A);
+              break;
+
+            case ' ':
+              $this->action(self::ACTION_DELETE_A_B);
+              break;
+
+            default:
+              if ($this->isAlphaNum($this->b)) {
+                $this->action(self::ACTION_KEEP_A);
+              }
+              else {
+                $this->action(self::ACTION_DELETE_A);
+              }
+          }
+          break;
+
+        default:
+          switch ($this->b) {
+            case ' ':
+              if ($this->isAlphaNum($this->a)) {
+                $this->action(self::ACTION_KEEP_A);
+                break;
+              }
+
+              $this->action(self::ACTION_DELETE_A_B);
+              break;
+
+            case "\n":
+              switch ($this->a) {
+                case '}':
+                case ']':
+                case ')':
+                case '+':
+                case '-':
+                case '"':
+                case "'":
+                  $this->action(self::ACTION_KEEP_A);
+                  break;
+
+                default:
+                  if ($this->isAlphaNum($this->a)) {
+                    $this->action(self::ACTION_KEEP_A);
+                  }
+                  else {
+                    $this->action(self::ACTION_DELETE_A_B);
+                  }
+              }
+              break;
+
+            default:
+              $this->action(self::ACTION_KEEP_A);
+              break;
+          }
+      }
+    }
+
+    return $this->output;
+  }
+
+  /**
+   * Get the next character, skipping over comments. peek() is used to see
+   *  if a '/' is followed by a '/' or '*'.
+   *
+   * @uses get()
+   * @uses peek()
+   * @throws JSMinException On unterminated comment.
+   * @return string
+   */
+  protected function next() {
+    $c = $this->get();
+
+    if ($c === '/') {
+      switch($this->peek()) {
+        case '/':
+          for (;;) {
+            $c = $this->get();
+
+            if (ord($c) <= self::ORD_LF) {
+              return $c;
+            }
+          }
+
+        case '*':
+          $this->get();
+
+          for (;;) {
+            switch($this->get()) {
+              case '*':
+                if ($this->peek() === '/') {
+                  $this->get();
+                  return ' ';
+                }
+                break;
+
+              case null:
+                throw new JSMinException('Unterminated comment.');
+            }
+          }
+
+        default:
+          return $c;
+      }
+    }
+
+    return $c;
+  }
+
+  /**
+   * Get next char. If is ctrl character, translate to a space or newline.
+   *
+   * @uses get()
+   * @return string|null
+   */
+  protected function peek() {
+    $this->lookAhead = $this->get();
+    return $this->lookAhead;
+  }
 }
+
+// -- Exceptions ---------------------------------------------------------------
+class JSMinException extends Exception {}
+
+
+?>
